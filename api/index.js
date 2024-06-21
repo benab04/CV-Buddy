f9c5f3cd9505dc70a2ea78f2e2f61ee19c4794a6
benab04 <benabrahambiju@gmail.com>
2024-06-21 17:54:43 +0530

    Add server for storing in real-time

---
+++ api/index.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const axios = require("axios")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config({})
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("Connected to database");
+}).catch(error => {
+    console.log("Error ==>", error);
+})
+
+const cvSchema = new mongoose.Schema({
+    data: String
+});
+const cvBuddy = mongoose.model('cvBuddy', cvSchema);
+
+app.get("/", (req, res) => {
+    res.send("Server is live");
+})
+
+app.post('/saveCv', async (req, res) => {
+    const { data } = req.body;
+    try {
+        let cv = await cvBuddy.findOne();
+        console.log("Save CV called");
+        if (cv) {
+            cv.data = data;
+            await cv.save();
+            res.json({ message: 'CV data updated successfully' });
+        } else {
+            const cvNew = new cvBuddy({ data });
+            await cvNew.save();
+            res.json({ message: 'CV data saved successfully' });
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'An error occurred', error });
+    }
+});
+// Endpoint to get CV data
+app.get('/getCv', async (req, res) => {
+    console.log("Get CV called");
+    const cvs = await cvBuddy.find({});
+    res.json({ data: JSON.stringify(cvs) });
+});
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 5000");
+})
--- iitkgp/main.js
+++ iitkgp/main.js
@@ -1,16 +1,59 @@
-
 function initCv() {
-    cvData = JSON.parse(localStorage.getItem('iitkgpCvData'));
-    if(cvData){
-        cv.innerHTML = cvData;
-    }
+    fetch('http://localhost:3000/getCv')
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                let cvData = JSON.parse(data.data);
+                console.log(cvData);
+                if (cvData)// Assuming there is only one CV entry
+                    cv.innerHTML = cvData[0].data;
+            }
+        })
+        .catch(error => console.error('Error fetching CV data:', error));
 }
 
 initCv();
 
+function saveCvData() {
+    const cvData = cv.innerHTML;
+
+    fetch('http://localhost:3000/saveCv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: cvData })
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+        })
+        .catch(error => console.error('Error saving CV data:', error));
+}
+
+let lastCvData = cv.innerHTML;
+setInterval(() => {
+    const currentCvData = cv.innerHTML;
+    if (currentCvData !== lastCvData) {
+        lastCvData = currentCvData;
+        saveCvData();
+    }
+}, 5000);
+
 $('#saveCv').on('click', (event) => {
     cv.removeAttribute('contenteditable');
-    localStorage.setItem('iitkgpCvData', JSON.stringify(cv.innerHTML));
+    const cvData = cv.innerHTML;
+
+    fetch('http://localhost:5000/saveCv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: cvData })
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+        })
+        .catch(error => console.error('Error saving CV data:', error));
 });
 
 $('#resetCv').on('click', (event) => {
